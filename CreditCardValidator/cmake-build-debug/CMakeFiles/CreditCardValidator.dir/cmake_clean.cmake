b40a0f0fb8c119860096277d0d4f715d47f94ffd
Santhosh Mamidisetti <92091342+SANTHOSH-MAMIDISETTI@users.noreply.github.com>
2022-08-09 17:51:32 +0530

    Create README.md

---
+++ CreditCardValidator/cmake-build-debug/CMakeFiles/CreditCardValidator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CreditCardValidator.dir/main.cpp.obj"
+  "CMakeFiles/CreditCardValidator.dir/main.cpp.obj.d"
+  "CreditCardValidator.exe"
+  "CreditCardValidator.exe.manifest"
+  "CreditCardValidator.pdb"
+  "libCreditCardValidator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CreditCardValidator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- CreditCardValidator/cmake-build-debug/cmake_install.cmake
+++ CreditCardValidator/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/from_github/CreditCardValidator
+# Install script for directory: E:/from_github/CreditCardValidator/CreditCardValidator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/from_github/CreditCardValidator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/from_github/CreditCardValidator/CreditCardValidator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
